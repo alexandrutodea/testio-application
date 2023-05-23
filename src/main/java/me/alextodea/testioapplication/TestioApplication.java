514e9f81b76524470311b91aaee9bd1fcfb96bbe
alexandrutodea <alexandru.todea01@e-uvt.ro>
2023-05-23 18:11:09 +0300

    Initial commit

---
+++ src/main/java/me/alextodea/testioapplication/TestioApplication.java
@@ -0,0 +1,13 @@
+package me.alextodea.testioapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestioApplication.class, args);
+	}
+
+}
