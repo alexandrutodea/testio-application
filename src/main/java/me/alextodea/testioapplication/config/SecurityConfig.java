d3cb40ee694b115dad231b80ac96b6ccc70ca01c
alexandrutodea <alexandru.todea01@e-uvt.ro>
2023-05-23 18:18:44 +0300

    Added SecurityConfig

---
+++ src/main/java/me/alextodea/testioapplication/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package me.alextodea.testioapplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final LogoutHandler logoutHandler;
+
+    public SecurityConfig(LogoutHandler logoutHandler) {
+        this.logoutHandler = logoutHandler;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable().
+                authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .addLogoutHandler(logoutHandler);
+
+        return http.build();
+    }
+
+// If using HS256, create a Bean to specify the HS256 should be used. By default, RS256 will be used.
+//    @Bean
+//    public JwtDecoderFactory<ClientRegistration> idTokenDecoderFactory() {
+//        OidcIdTokenDecoderFactory idTokenDecoderFactory = new OidcIdTokenDecoderFactory();
+//        idTokenDecoderFactory.setJwsAlgorithmResolver(clientRegistration -> MacAlgorithm.HS256);
+//        return idTokenDecoderFactory;
+//    }
+}
